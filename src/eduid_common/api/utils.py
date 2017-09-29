5067aef8158be3f74322027342b9d75a96ee28c9
Enrique Pérez Arnaud <enrique@cazalla.net>
2017-09-29 14:27:03 +0200

    common save_user function

--- src/eduid_common/api/utils.py
+++ src/eduid_common/api/utils.py
@@ -113,6 +113,23 @@ def save_dashboard_user(user):
     return current_app.am_relay.request_user_sync(user)
 
 
+def save_user(user):
+    """
+    Save (new) user objects to the db in the new format,
+    and propagate the changes to the central user db.
+
+    May raise UserOutOfSync exception
+
+    :param user: the modified user
+    :type user: eduid_userdb.ProofingUser
+    """
+    if isinstance(user, User) and not isinstance(user, ProofingUser):
+        # turn it into a ProofingUser before saving it in the dashboard private db
+        user = ProofingUser(data = user.to_dict())
+    current_app.proofing_userdb.save(user)
+    return current_app.am_relay.request_user_sync(user)
+
+
 def urlappend(base, path):
     """
     :param base: Base url
